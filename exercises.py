af68ec5091193460c18b522dca63e5a14b10433a
Andy Aschwanden <aaschwanden@alaska.edu>
2014-07-31 19:25:32 -0800

    Two Will & Carl exercises in Glacier Dynamics

---
+++ exercises.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+
+import numpy as np
+
+rho = 910
+g = 9.81
+alphas = [1, 2, 5, 10]
+A = 2.4e-24
+
+secpera = 31556926
+
+R = 2000
+
+u_def_channel = 2 * A * (1/2 * rho * g * np.sin(alpha)**n * ((R**(n+1))/(n+1))
